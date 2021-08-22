57987f4782b84bcff0a8760626feaa8ae4d43c2e
dnwn306 <dnwn306@naver.com>
2021-08-22 21:44:27 +0900

    2021-08-22

--- Shell/app/src/main/java/com/example/shell/MainActivity.java
+++ Shell/app/src/main/java/com/example/shell/MainActivity.java
@@ -1,33 +1,90 @@
 package com.example.shell;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtView;
     EditText editText;
+    ScrollView scrollView;
 
+    private static String cmd_result;
+    private static Handler mHandler;
+    private static Process p = null;
+
+
+    private void checkStoragePermission(){
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permissionCheck == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(permissionCheck == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         txtView = (TextView) findViewById(R.id.txtView);
         editText = (EditText) findViewById(R.id.editText);
+        scrollView = (ScrollView) findViewById(R.id.scrollView) ;
+
+
+        // 저장소 권한 확인
+        checkStoragePermission();
+
+
+
+        // 화면 출력 역할 핸들러
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+
+                // Clear
+                if( cmd_result.equals(new String("\u001B[2J\u001B[H")) )
+                    txtView.setText("");
+                else
+                    txtView.setText(txtView.getText() + cmd_result);
+
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                editText.requestFocus();
+
+            }
+
+        };
+        
+        
+
+        ExampleThread thread = new ExampleThread();
+        thread.start();
+
 
 
 
@@ -40,9 +97,17 @@ public class MainActivity extends AppCompatActivity {
 
 
                     String cmd = editText.getText().toString();
-                    RunCommand( cmd );
-                    editText.setText(null);
 
+
+                    // 입력 신호를 쉘에게 리다이렉트
+                    PrintWriter writer = new PrintWriter(  p.getOutputStream() );
+                    writer.println( cmd );
+                    writer.flush();
+
+
+                    editText.setText(null);
+                    scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                    editText.requestFocus();
 
                 }
                 else
@@ -60,44 +125,115 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void RunCommand(String cmd){
 
-        java.lang.Process process = null;
-        InputStreamReader inputReader = null;
-        BufferedReader bufferedReader = null;
+    // 스레드
+    private class ExampleThread extends Thread{
+        public ExampleThread(){
 
-        try {
-
-
-            process = Runtime.getRuntime().exec(cmd);
-            process.waitFor();
+        }
+        public void run(){
 
 
-            inputReader = new InputStreamReader(process.getInputStream(), "MS949");
-            bufferedReader = new BufferedReader(inputReader);
+            ProcessBuilder pb = new ProcessBuilder("/system/bin/sh");
 
-            String content = "";
-            String line = "";
-            while( (line = bufferedReader.readLine()) != null ){
-                if(line.trim().length() == 0)
-                    continue;
-                content += line + "\n";
+            // 시작 경로
+            pb.directory( new File("/storage/emulated/0/") );
+
+
+            // 프로세스 시작
+            try {
+                p = pb.start();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            txtView.setText(content);
 
-            int resultValue = process.exitValue();
 
-            inputReader.close();
-            bufferedReader.close();
-            //process.destroy();
 
-        } catch (Exception e){
-            txtView.setText("Exception: "+e.toString());
+
+            // 표준 출력 리다이렉션
+            InputStream stdOut = p.getInputStream();
+
+            new Thread(new Runnable(){
+                public void run(){
+                    byte[] buffer = new byte[8192];
+                    int len = -1;
+                    try {
+                        while((len = stdOut.read(buffer)) > 0){
+
+
+                            String str = new String();
+
+                            str += new String(buffer, 0, len, "UTF-8");
+
+
+                            cmd_result = str;
+                            mHandler.sendEmptyMessage(0);
+
+
+                        }
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+
+
+                    }
+                }
+            }).start();
+
+
+
+            // 표준 에러 리다이렉션
+            InputStream stdError = p.getErrorStream();
+
+            new Thread(new Runnable(){
+                public void run(){
+                    byte[] buffer = new byte[8192];
+                    int len = -1;
+                    try {
+
+                        while((len = stdError.read(buffer)) > 0){
+
+
+                            String str = new String();
+
+                            str+= new String(buffer, 0, len, "UTF-8");
+
+                            cmd_result = str;
+                            mHandler.sendEmptyMessage(0);
+
+
+
+
+
+                        }
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+
+
+                    }
+                }
+            }).start();
+
+
+
+
+
+            // 프로세스가 끝날때 까지 기다림.
+            try {
+                p.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // 앱 종료
+            finish();
+
+
+
         }
-
     }
-
 
 
 }
